afff86ec96be014e6f23663db0e39cd2908bd704
lexyspyrou <alexandra.spyrou@outlook.com>
2022-06-15 23:50:19 +0100

    fixed bug that commented out the last arguments of proportional.sh.

--- job_scripts/related_ted8_o2m/proportional.sh
+++ job_scripts/related_ted8_o2m/proportional.sh
@@ -4,6 +4,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --time=0
 #SBATCH --mem=15GB
+# --max-tokens 4800
 
 MODEL_DIR=checkpoints/related_ted8_o2m/proportional/
 mkdir -p $MODEL_DIR
@@ -16,8 +17,8 @@ python train.py data-bin/ted_8_related/ \
 	  --task multilingual_translation \
 	  --arch multilingual_transformer_iwslt_de_en \
 	  --max-epoch 40 \
-          --dataset-type "multi" \
-    	  --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
+    --dataset-type "multi" \
+    --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
 	  --no-epoch-checkpoints \
 	  --distributed-world-size 1 \
 	  --encoder-langtok "tgt" \
@@ -27,7 +28,7 @@ python train.py data-bin/ted_8_related/ \
 	  --optimizer 'adam' --adam-betas '(0.9, 0.98)' --lr-scheduler 'inverse_sqrt_decay' \
 	  --warmup-init-lr 1e-7 --warmup-updates 4000 --lr 2e-4 --lr-shrink 0.8 \
 	  --criterion 'label_smoothed_cross_entropy' --label-smoothing 0.1 \
-	  --max-tokens 1000 #4800 \
+	  --max-tokens 1000 \
 	  --update-freq 2 \
 	  --seed 2 \
   	  --max-source-positions 150 --max-target-positions 150 \
