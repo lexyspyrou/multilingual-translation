d5ed2a047540b8cf2ce1f7501fd9f10f085c145b
lexyspyrou <alexandra.spyrou@outlook.com>
2022-06-16 01:02:51 +0100

    train 2 languages m2o

--- job_scripts/related_ted8_m2o/proportional.sh
+++ job_scripts/related_ted8_m2o/proportional.sh
@@ -16,22 +16,23 @@ python train.py data-bin/ted_8_related/ \
 	  --task multilingual_translation \
 	  --arch multilingual_transformer_iwslt_de_en \
 	  --max-epoch 40 \
-          --dataset-type "multi" \
-          --lang-pairs "aze-eng,tur-eng,bel-eng,rus-eng,glg-eng,por-eng,slk-eng,ces-eng" \
+    --dataset-type "multi" \
+    --lang-pairs "bel-eng,rus-eng" \
 	  --no-epoch-checkpoints \
 	  --distributed-world-size 1 \
+	  --encoder-langtok "tgt" \
 	  --share-decoder-input-output-embed --share-decoders --share-encoders \
 	  --dropout 0.3 --attention-dropout 0.3 --relu-dropout 0.3 --weight-decay 0.0 \
 	  --left-pad-source 'True' --left-pad-target 'False' \
 	  --optimizer 'adam' --adam-betas '(0.9, 0.98)' --lr-scheduler 'inverse_sqrt_decay' \
 	  --warmup-init-lr 1e-7 --warmup-updates 4000 --lr 2e-4 --lr-shrink 0.8 \
 	  --criterion 'label_smoothed_cross_entropy' --label-smoothing 0.1 \
-	  --max-tokens 4800 \
+	  --max-tokens 1000 \
 	  --update-freq 2 \
 	  --seed 2 \
-  	  --max-source-positions 150 --max-target-positions 150 \
-  	  --save-dir $MODEL_DIR \
-          --encoder-normalize-before --decoder-normalize-before \
-          --scale-norm \
-          --datasize-t 1 \
+    --max-source-positions 150 --max-target-positions 150 \
+    --save-dir $MODEL_DIR \
+    --encoder-normalize-before --decoder-normalize-before \
+    --scale-norm \
+    --datasize-t 1 \
 	  --log-interval 100 >> $MODEL_DIR/train.log 2>&1
