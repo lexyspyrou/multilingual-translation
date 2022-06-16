6c3652dfec15975ecae94666675b95a7efec60f7
lexyspyrou <alexandra.spyrou@outlook.com>
2022-06-16 01:04:00 +0100

    decode 2 languages o2m

--- job_scripts/related_ted8_o2m/trans.sh
+++ job_scripts/related_ted8_o2m/trans.sh
@@ -4,9 +4,35 @@
 #SBATCH --gres=gpu:1
 #SBATCH --time=0
 #SBATCH --mem=15GB
+#     	  --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
 
 OUTDIR=$1
+echo $OUTDIR
+#python generate.py data-bin/ted_8_related/ \
+#          --task multilingual_translation \
+#          --gen-subset test \
+#          --path "$OUTDIR"/checkpoint_best.pt \
+#          --batch-size 32 \
+#          --lenpen 1.0 \
+#          --remove-bpe sentencepiece \
+#	  --sacrebleu \
+#    	  --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
+#          --source-lang eng --target-lang aze \
+#          --beam 5   > "$OUTDIR"/test_engaze.log
+#
+#python generate.py data-bin/ted_8_related/ \
+#          --task multilingual_translation \
+#          --gen-subset test \
+#          --path "$OUTDIR"/checkpoint_best.pt \
+#          --batch-size 32 \
+#          --lenpen 1.0 \
+#          --remove-bpe sentencepiece \
+#	  --sacrebleu \
+#    	  --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
+#          --source-lang eng --target-lang tur \
+#          --beam 5   > "$OUTDIR"/test_engtur.log
 
+#FIXME: Added --encoder-langtok "tgt". Is it correct choice ?
 python generate.py data-bin/ted_8_related/ \
           --task multilingual_translation \
           --gen-subset test \
@@ -14,34 +40,11 @@ python generate.py data-bin/ted_8_related/ \
           --batch-size 32 \
           --lenpen 1.0 \
           --remove-bpe sentencepiece \
-	  --sacrebleu \
-    	  --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
-          --source-lang eng --target-lang aze \
-          --beam 5   > "$OUTDIR"/test_engaze.log
-
-python generate.py data-bin/ted_8_related/ \
-          --task multilingual_translation \
-          --gen-subset test \
-          --path "$OUTDIR"/checkpoint_best.pt \
-          --batch-size 32 \
-          --lenpen 1.0 \
-          --remove-bpe sentencepiece \
-	  --sacrebleu \
-    	  --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
-          --source-lang eng --target-lang tur \
-          --beam 5   > "$OUTDIR"/test_engtur.log
-
-python generate.py data-bin/ted_8_related/ \
-          --task multilingual_translation \
-          --gen-subset test \
-          --path "$OUTDIR"/checkpoint_best.pt \
-          --batch-size 32 \
-          --lenpen 1.0 \
-          --remove-bpe sentencepiece \
-	  --sacrebleu \
-    	  --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
+          --encoder-langtok "tgt" \
+	        --sacrebleu \
+      	  --lang-pairs "eng-bel, eng-bel" \
           --source-lang eng --target-lang bel \
-          --beam 5   > "$OUTDIR"/test_engbel.log
+          --beam 5  > "$OUTDIR"/test_engbel.log
 
 python generate.py data-bin/ted_8_related/ \
           --task multilingual_translation \
@@ -51,55 +54,55 @@ python generate.py data-bin/ted_8_related/ \
           --lenpen 1.0 \
           --remove-bpe sentencepiece \
 	  --sacrebleu \
-    	  --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
+    	  --lang-pairs "eng-bel,eng-rus" \
           --source-lang eng --target-lang rus \
           --beam 5   > "$OUTDIR"/test_engrus.log
-
-python generate.py data-bin/ted_8_related/ \
-          --task multilingual_translation \
-          --gen-subset test \
-          --path "$OUTDIR"/checkpoint_best.pt \
-          --batch-size 32 \
-          --lenpen 1.0 \
-          --remove-bpe sentencepiece \
-	  --sacrebleu \
-    	  --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
-          --source-lang eng --target-lang glg \
-          --beam 5   > "$OUTDIR"/test_engglg.log
-
-python generate.py data-bin/ted_8_related/ \
-          --task multilingual_translation \
-          --gen-subset test \
-          --path "$OUTDIR"/checkpoint_best.pt \
-          --batch-size 32 \
-          --lenpen 1.0 \
-          --remove-bpe sentencepiece \
-	  --sacrebleu \
-    	  --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
-          --source-lang eng --target-lang por \
-          --beam 5   > "$OUTDIR"/test_engpor.log
-
-python generate.py data-bin/ted_8_related/ \
-          --task multilingual_translation \
-          --gen-subset test \
-          --path "$OUTDIR"/checkpoint_best.pt \
-          --batch-size 32 \
-          --lenpen 1.0 \
-          --remove-bpe sentencepiece \
-	  --sacrebleu \
-    	  --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
-          --source-lang eng --target-lang slk \
-          --beam 5   > "$OUTDIR"/test_engslk.log
-
-python generate.py data-bin/ted_8_related/ \
-          --task multilingual_translation \
-          --gen-subset test \
-          --path "$OUTDIR"/checkpoint_best.pt \
-          --batch-size 32 \
-          --lenpen 1.0 \
-          --remove-bpe sentencepiece \
-	  --sacrebleu \
-    	  --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
-          --source-lang eng --target-lang ces \
-          --beam 5   > "$OUTDIR"/test_engces.log
-
+#
+#python generate.py data-bin/ted_8_related/ \
+#          --task multilingual_translation \
+#          --gen-subset test \
+#          --path "$OUTDIR"/checkpoint_best.pt \
+#          --batch-size 32 \
+#          --lenpen 1.0 \
+#          --remove-bpe sentencepiece \
+#	  --sacrebleu \
+#    	  --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
+#          --source-lang eng --target-lang glg \
+#          --beam 5   > "$OUTDIR"/test_engglg.log
+#
+#python generate.py data-bin/ted_8_related/ \
+#          --task multilingual_translation \
+#          --gen-subset test \
+#          --path "$OUTDIR"/checkpoint_best.pt \
+#          --batch-size 32 \
+#          --lenpen 1.0 \
+#          --remove-bpe sentencepiece \
+#	  --sacrebleu \
+#    	  --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
+#          --source-lang eng --target-lang por \
+#          --beam 5   > "$OUTDIR"/test_engpor.log
+#
+#python generate.py data-bin/ted_8_related/ \
+#          --task multilingual_translation \
+#          --gen-subset test \
+#          --path "$OUTDIR"/checkpoint_best.pt \
+#          --batch-size 32 \
+#          --lenpen 1.0 \
+#          --remove-bpe sentencepiece \
+#	  --sacrebleu \
+#    	  --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
+#          --source-lang eng --target-lang slk \
+#          --beam 5   > "$OUTDIR"/test_engslk.log
+#
+#python generate.py data-bin/ted_8_related/ \
+#          --task multilingual_translation \
+#          --gen-subset test \
+#          --path "$OUTDIR"/checkpoint_best.pt \
+#          --batch-size 32 \
+#          --lenpen 1.0 \
+#          --remove-bpe sentencepiece \
+#	  --sacrebleu \
+#    	  --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
+#          --source-lang eng --target-lang ces \
+#          --beam 5   > "$OUTDIR"/test_engces.log
+#
