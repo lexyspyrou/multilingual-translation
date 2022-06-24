0a76c8f9d092760f1f3a4296350aa5f63c07331c
lexyspyrou <alexandra.spyrou@outlook.com>
2022-06-24 13:28:00 +0100

    fix arguments in proportional.sh

--- job_scripts/related_ted8_o2m/proportional.sh
+++ job_scripts/related_ted8_o2m/proportional.sh
@@ -17,7 +17,6 @@ python train.py data-bin/ted_8_related/ \
 	  --task multilingual_translation \
 	  --arch multilingual_transformer_iwslt_de_en \
 	  --max-epoch 40 \
-    --dataset-type "multi" \
     --lang-pairs "eng-bel,eng-rus" \
 	  --no-epoch-checkpoints \
 	  --distributed-world-size 1 \
@@ -25,8 +24,8 @@ python train.py data-bin/ted_8_related/ \
 	  --share-decoder-input-output-embed --share-decoders --share-encoders \
 	  --dropout 0.3 --attention-dropout 0.3 --relu-dropout 0.3 --weight-decay 0.0 \
 	  --left-pad-source 'True' --left-pad-target 'False' \
-	  --optimizer 'adam' --adam-betas '(0.9, 0.98)' --lr-scheduler 'inverse_sqrt_decay' \
-	  --warmup-init-lr 1e-7 --warmup-updates 4000 --lr 2e-4 --lr-shrink 0.8 \
+	  --optimizer 'adam' --adam-betas '(0.9, 0.98)' --lr-scheduler 'inverse_sqrt' \
+	  --warmup-init-lr 1e-7 --warmup-updates 4000 --lr 2e-4 \
 	  --criterion 'label_smoothed_cross_entropy' --label-smoothing 0.1 \
 	  --max-tokens 1000 \
 	  --update-freq 2 \
@@ -34,7 +33,12 @@ python train.py data-bin/ted_8_related/ \
   	--max-source-positions 150 --max-target-positions 150 \
   	--save-dir $MODEL_DIR \
     --encoder-normalize-before --decoder-normalize-before \
-    --scale-norm \
-    --datasize-t 1 \
 	  --log-interval 100 >> $MODEL_DIR/train.log 2>&1 \
+	  --encoder-layers 4 \
+    --decoder-layers 4 \
+    --encoder-embed-dim 256 \
+    --decoder-embed-dim 256 \
+    --encoder-ffn-embed-dim 512 \
+    --decoder-ffn-embed-dim 512 \
+    --skip-invalid-size-inputs-valid-test
 
