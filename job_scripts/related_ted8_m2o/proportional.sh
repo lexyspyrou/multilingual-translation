ce4ce0ef81a2f422d865db15bf829c2f6c04e88b
lexyspyrou <alexandra.spyrou@outlook.com>
2022-07-04 22:09:16 +0100

    to test on server

--- job_scripts/related_ted8_m2o/proportional.sh
+++ job_scripts/related_ted8_m2o/proportional.sh
@@ -11,7 +11,7 @@ echo 'slurm id '$SLURM_JOB_ID >> $MODEL_DIR/train.log
 python train.py data-bin/ted_8_related/ \
 	  --task multilingual_translation \
 	  --arch multilingual_transformer_iwslt_de_en \
-	  --max-epoch 25 \
+	  --max-epoch 1 \
     --lang-pairs "bel-eng,rus-eng" \
 	  --no-epoch-checkpoints \
 	  --distributed-world-size 4 \
@@ -22,7 +22,7 @@ python train.py data-bin/ted_8_related/ \
 	  --optimizer 'adam' --adam-betas '(0.9, 0.98)' --lr-scheduler 'inverse_sqrt' \
 	  --warmup-init-lr 1e-7 --warmup-updates 4000 --lr 2e-4 \
 	  --criterion 'label_smoothed_cross_entropy' --label-smoothing 0.1 \
-	  --max-tokens 1000 \
+	  --max-tokens 4800 \
 	  --update-freq 2 \
 	  --seed 2 \
     --max-source-positions 150 --max-target-positions 150 \
