f93076a98800c3bd19af16ced0ced31de694392d
lexyspyrou <alexandra.spyrou@outlook.com>
2022-07-04 23:47:04 +0100

    to test on server

--- job_scripts/related_ted8_m2o/proportional.sh
+++ job_scripts/related_ted8_m2o/proportional.sh
@@ -14,7 +14,7 @@ python train.py data-bin/ted_8_related/ \
 	  --max-epoch 1 \
     --lang-pairs "bel-eng,rus-eng" \
 	  --no-epoch-checkpoints \
-	  --distributed-world-size 4 \
+	  --distributed-world-size 1 \
 	  --encoder-langtok "tgt" \
 	  --share-decoder-input-output-embed --share-decoders --share-encoders \
 	  --dropout 0.3 --attention-dropout 0.3 --relu-dropout 0.3 --weight-decay 0.0 \
@@ -22,7 +22,7 @@ python train.py data-bin/ted_8_related/ \
 	  --optimizer 'adam' --adam-betas '(0.9, 0.98)' --lr-scheduler 'inverse_sqrt' \
 	  --warmup-init-lr 1e-7 --warmup-updates 4000 --lr 2e-4 \
 	  --criterion 'label_smoothed_cross_entropy' --label-smoothing 0.1 \
-	  --max-tokens 4800 \
+	  --max-tokens 1000 \
 	  --update-freq 2 \
 	  --seed 2 \
     --max-source-positions 150 --max-target-positions 150 \
