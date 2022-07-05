1f6394843a3ddaf7689bf6f1b458ef45df421fca
lexyspyrou <alexandra.spyrou@outlook.com>
2022-07-05 22:29:16 +0100

    fixing structure of arguments in proportional.sh

--- job_scripts/diverse_ted8_m2o/proportional.sh
+++ job_scripts/diverse_ted8_m2o/proportional.sh
@@ -26,12 +26,12 @@ python train.py data-bin/ted_8_diverse/ \
 	  --optimizer 'adam' --adam-betas '(0.9, 0.98)' --lr-scheduler 'inverse_sqrt_decay' \
 	  --warmup-init-lr 1e-7 --warmup-updates 4000 --lr 2e-4 --lr-shrink 0.8 \
 	  --criterion 'label_smoothed_cross_entropy' --label-smoothing 0.1 \
-	  --max-tokens 3000 \   #4800
+	  --max-tokens 3000 \
 	  --update-freq 2 \
 	  --seed 2 \
-  	  --max-source-positions 150 --max-target-positions 150 \
-  	  --save-dir $MODEL_DIR \
-          --encoder-normalize-before --decoder-normalize-before \
-          --scale-norm \
-          --datasize-t 1 \
+    --max-source-positions 150 --max-target-positions 150 \
+    --save-dir $MODEL_DIR \
+    --encoder-normalize-before --decoder-normalize-before \
+    --scale-norm \
+    --datasize-t 1 \
 	  --log-interval 100 >> $MODEL_DIR/train.log 2>&1
