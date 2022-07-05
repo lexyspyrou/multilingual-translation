c5064d79d978235c8621c574b507e152d32cbdfe
lexyspyrou <alexandra.spyrou@outlook.com>
2022-07-05 23:28:45 +0100

    updating o2m proportional to train all languages. Small model.

--- job_scripts/related_ted8_o2m/proportional.sh
+++ job_scripts/related_ted8_o2m/proportional.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-#SBATCH --nodes=1
-#SBATCH --gres=gpu:1
-#SBATCH --time=0
-#SBATCH --mem=15GB
-# --max-tokens 4800
-#     --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
 MODEL_DIR=checkpoints/related_ted8_o2m/proportional/
 mkdir -p $MODEL_DIR
 
@@ -16,10 +9,10 @@ echo 'slurm id '$SLURM_JOB_ID >> $MODEL_DIR/train.log
 python train.py data-bin/ted_8_related/ \
 	  --task multilingual_translation \
 	  --arch multilingual_transformer_iwslt_de_en \
-	  --max-epoch 40 \
-    --lang-pairs "eng-bel,eng-rus" \
-	  --no-epoch-checkpoints \
-	  --distributed-world-size 1 \
+	  --max-epoch 1 \
+    --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
+    --no-epoch-checkpoints \
+	  --distributed-world-size 4 \
 	  --encoder-langtok "tgt" \
 	  --share-decoder-input-output-embed --share-decoders --share-encoders \
 	  --dropout 0.3 --attention-dropout 0.3 --relu-dropout 0.3 --weight-decay 0.0 \
@@ -27,18 +20,18 @@ python train.py data-bin/ted_8_related/ \
 	  --optimizer 'adam' --adam-betas '(0.9, 0.98)' --lr-scheduler 'inverse_sqrt' \
 	  --warmup-init-lr 1e-7 --warmup-updates 4000 --lr 2e-4 \
 	  --criterion 'label_smoothed_cross_entropy' --label-smoothing 0.1 \
-	  --max-tokens 1000 \
+	  --max-tokens 4800 \
 	  --update-freq 2 \
 	  --seed 2 \
-  	--max-source-positions 150 --max-target-positions 150 \
-  	--save-dir $MODEL_DIR \
+    --max-source-positions 150 --max-target-positions 150 \
+    --save-dir $MODEL_DIR \
     --encoder-normalize-before --decoder-normalize-before \
 	  --log-interval 100 >> $MODEL_DIR/train.log 2>&1 \
-	  --encoder-layers 4 \
+    --encoder-layers 4 \
     --decoder-layers 4 \
     --encoder-embed-dim 256 \
     --decoder-embed-dim 256 \
     --encoder-ffn-embed-dim 512 \
     --decoder-ffn-embed-dim 512 \
-    --skip-invalid-size-inputs-valid-test
-
+    --skip-invalid-size-inputs-valid-test \
+    --ddp-backend=no_c10d
