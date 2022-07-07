ef7bf33e381ff882bcfc8ca4b9f8ade7de9d05e5
lexyspyrou <alexandra.spyrou@outlook.com>
2022-07-07 17:50:23 +0100

    fixing ted8diverse args

--- job_scripts/diverse_ted8_o2m/proportional.sh
+++ job_scripts/diverse_ted8_o2m/proportional.sh
@@ -15,24 +15,29 @@ echo 'slurm id '$SLURM_JOB_ID >> $MODEL_DIR/train.log
 python train.py data-bin/ted_8_diverse/ \
 	  --task multilingual_translation \
 	  --arch multilingual_transformer_iwslt_de_en \
-	  --max-epoch 40 \
-          --dataset-type "multi" \
-          --lang-pairs "eng-bos,eng-mar,eng-hin,eng-mkd,eng-ell,eng-bul,eng-fra,eng-kor" \
+	  --max-epoch 1 \
+    --lang-pairs "eng-bos,eng-mar,eng-hin,eng-mkd,eng-ell,eng-bul,eng-fra,eng-kor" \
 	  --no-epoch-checkpoints \
 	  --distributed-world-size 1 \
 	  --encoder-langtok "tgt" \
 	  --share-decoder-input-output-embed --share-decoders --share-encoders \
 	  --dropout 0.3 --attention-dropout 0.3 --relu-dropout 0.3 --weight-decay 0.0 \
 	  --left-pad-source 'True' --left-pad-target 'False' \
-	  --optimizer 'adam' --adam-betas '(0.9, 0.98)' --lr-scheduler 'inverse_sqrt_decay' \
-	  --warmup-init-lr 1e-7 --warmup-updates 4000 --lr 2e-4 --lr-shrink 0.8 \
+	  --optimizer 'adam' --adam-betas '(0.9, 0.98)' --lr-scheduler 'inverse_sqrt' \
+	  --warmup-init-lr 1e-7 --warmup-updates 4000 --lr 2e-4 \
 	  --criterion 'label_smoothed_cross_entropy' --label-smoothing 0.1 \
-	  --max-tokens 4800 \
+	  --max-tokens 1000 \
 	  --update-freq 2 \
 	  --seed 2 \
-  	  --max-source-positions 150 --max-target-positions 150 \
-  	  --save-dir $MODEL_DIR \
-          --encoder-normalize-before --decoder-normalize-before \
-          --scale-norm \
-          --datasize-t 1 \
-	  --log-interval 100 >> $MODEL_DIR/train.log 2>&1
+    --max-source-positions 150 --max-target-positions 150 \
+    --save-dir $MODEL_DIR \
+    --encoder-normalize-before --decoder-normalize-before \
+	  --log-interval 100 >> $MODEL_DIR/train.log 2>&1 \
+    --skip-invalid-size-inputs-valid-test \
+    --ddp-backend=no_c10d \
+    --encoder-layers 4 \
+    --decoder-layers 4 \
+    --encoder-embed-dim 256 \
+    --decoder-embed-dim 256 \
+    --encoder-ffn-embed-dim 512 \
+    --decoder-ffn-embed-dim 512
