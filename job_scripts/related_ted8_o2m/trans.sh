a5a05399316e8b8eee68b049b06c46d0a8a65d69
lexyspyrou <alexandra.spyrou@outlook.com>
2022-07-09 19:10:59 +0100

    back to original trans.sh for all language pairs

--- job_scripts/related_ted8_o2m/trans.sh
+++ job_scripts/related_ted8_o2m/trans.sh
@@ -19,7 +19,7 @@ python fairseq_cli/generate.py data-bin/ted_8_related/ \
 	        --sacrebleu \
           --encoder-langtok tgt \
 	        --skip-invalid-size-inputs-valid-test \
-    	    --lang-pairs "eng-aze,eng-tur" \
+    	    --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
           --source-lang eng --target-lang aze \
           --beam 5   > "$OUTDIR"/test_engaze.log
 
@@ -33,92 +33,92 @@ python fairseq_cli/generate.py data-bin/ted_8_related/ \
 	        --sacrebleu \
           --encoder-langtok tgt \
 	        --skip-invalid-size-inputs-valid-test \
-    	    --lang-pairs "eng-aze,eng-tur" \
+    	    --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
           --source-lang eng --target-lang tur \
           --beam 5   > "$OUTDIR"/test_engtur.log
 
 #FIXME: Added --encoder-langtok "tgt" and --skip-invalid-size-inputs-valid-test \. Is it correct choice ?
-#python fairseq_cli/generate.py data-bin/ted_8_related/ \
-#          --task multilingual_translation \
-#          --gen-subset test \
-#          --path "$OUTDIR"/checkpoint_best.pt \
-#          --batch-size 32 \
-#          --lenpen 1.0 \
-#          --remove-bpe sentencepiece \
-#	        --sacrebleu \
-#          --encoder-langtok tgt \
-#	        --skip-invalid-size-inputs-valid-test \
-#      	  --lang-pairs "eng-aze,eng-tur" \
-#          --source-lang eng --target-lang bel \
-#          --beam 5  > "$OUTDIR"/test_engbel.log
-#
-#python fairseq_cli/generate.py data-bin/ted_8_related/ \
-#          --task multilingual_translation \
-#          --gen-subset test \
-#          --path "$OUTDIR"/checkpoint_best.pt \
-#          --batch-size 32 \
-#          --lenpen 1.0 \
-#          --remove-bpe sentencepiece \
-#	        --sacrebleu \
-#          --encoder-langtok tgt \
-#	        --skip-invalid-size-inputs-valid-test \
-#    	    --lang-pairs "eng-aze,eng-tur" \
-#          --source-lang eng --target-lang rus \
-#          --beam 5   > "$OUTDIR"/test_engrus.log
-#
-#python fairseq_cli/generate.py data-bin/ted_8_related/ \
-#          --task multilingual_translation \
-#          --gen-subset test \
-#          --path "$OUTDIR"/checkpoint_best.pt \
-#          --batch-size 32 \
-#          --lenpen 1.0 \
-#          --remove-bpe sentencepiece \
-#	        --sacrebleu \
-#          --encoder-langtok tgt \
-#	        --skip-invalid-size-inputs-valid-test \
-#    	    --lang-pairs "eng-aze,eng-tur" \
-#          --source-lang eng --target-lang glg \
-#          --beam 5   > "$OUTDIR"/test_engglg.log
-#
-#python fairseq_cli/generate.py data-bin/ted_8_related/ \
-#          --task multilingual_translation \
-#          --gen-subset test \
-#          --path "$OUTDIR"/checkpoint_best.pt \
-#          --batch-size 32 \
-#          --lenpen 1.0 \
-#          --remove-bpe sentencepiece \
-#	        --sacrebleu \
-#          --encoder-langtok tgt \
-#	        --skip-invalid-size-inputs-valid-test \
-#    	    --lang-pairs "eng-aze,eng-tur" \
-#          --source-lang eng --target-lang por \
-#          --beam 5   > "$OUTDIR"/test_engpor.log
-#
-#python fairseq_cli/generate.py data-bin/ted_8_related/ \
-#          --task multilingual_translation \
-#          --gen-subset test \
-#          --path "$OUTDIR"/checkpoint_best.pt \
-#          --batch-size 32 \
-#          --lenpen 1.0 \
-#          --remove-bpe sentencepiece \
-#	        --sacrebleu \
-#          --encoder-langtok tgt \
-#	        --skip-invalid-size-inputs-valid-test \
-#    	    --lang-pairs "eng-aze,eng-tur" \
-#          --source-lang eng --target-lang slk \
-#          --beam 5   > "$OUTDIR"/test_engslk.log
-#
-#python fairseq_cli/generate.py data-bin/ted_8_related/ \
-#          --task multilingual_translation \
-#          --gen-subset test \
-#          --path "$OUTDIR"/checkpoint_best.pt \
-#          --batch-size 32 \
-#          --lenpen 1.0 \
-#          --remove-bpe sentencepiece \
-#	        --sacrebleu \
-#          --encoder-langtok tgt \
-#	        --skip-invalid-size-inputs-valid-test \
-#    	    --lang-pairs "eng-aze,eng-tur" \
-#          --source-lang eng --target-lang ces \
-#          --beam 5   > "$OUTDIR"/test_engces.log
-#
+python fairseq_cli/generate.py data-bin/ted_8_related/ \
+          --task multilingual_translation \
+          --gen-subset test \
+          --path "$OUTDIR"/checkpoint_best.pt \
+          --batch-size 32 \
+          --lenpen 1.0 \
+          --remove-bpe sentencepiece \
+	        --sacrebleu \
+          --encoder-langtok tgt \
+	        --skip-invalid-size-inputs-valid-test \
+      	  --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
+          --source-lang eng --target-lang bel \
+          --beam 5  > "$OUTDIR"/test_engbel.log
+
+python fairseq_cli/generate.py data-bin/ted_8_related/ \
+          --task multilingual_translation \
+          --gen-subset test \
+          --path "$OUTDIR"/checkpoint_best.pt \
+          --batch-size 32 \
+          --lenpen 1.0 \
+          --remove-bpe sentencepiece \
+	        --sacrebleu \
+          --encoder-langtok tgt \
+	        --skip-invalid-size-inputs-valid-test \
+    	    --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
+          --source-lang eng --target-lang rus \
+          --beam 5   > "$OUTDIR"/test_engrus.log
+
+python fairseq_cli/generate.py data-bin/ted_8_related/ \
+          --task multilingual_translation \
+          --gen-subset test \
+          --path "$OUTDIR"/checkpoint_best.pt \
+          --batch-size 32 \
+          --lenpen 1.0 \
+          --remove-bpe sentencepiece \
+	        --sacrebleu \
+          --encoder-langtok tgt \
+	        --skip-invalid-size-inputs-valid-test \
+    	    --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
+          --source-lang eng --target-lang glg \
+          --beam 5   > "$OUTDIR"/test_engglg.log
+
+python fairseq_cli/generate.py data-bin/ted_8_related/ \
+          --task multilingual_translation \
+          --gen-subset test \
+          --path "$OUTDIR"/checkpoint_best.pt \
+          --batch-size 32 \
+          --lenpen 1.0 \
+          --remove-bpe sentencepiece \
+	        --sacrebleu \
+          --encoder-langtok tgt \
+	        --skip-invalid-size-inputs-valid-test \
+    	    --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
+          --source-lang eng --target-lang por \
+          --beam 5   > "$OUTDIR"/test_engpor.log
+
+python fairseq_cli/generate.py data-bin/ted_8_related/ \
+          --task multilingual_translation \
+          --gen-subset test \
+          --path "$OUTDIR"/checkpoint_best.pt \
+          --batch-size 32 \
+          --lenpen 1.0 \
+          --remove-bpe sentencepiece \
+	        --sacrebleu \
+          --encoder-langtok tgt \
+	        --skip-invalid-size-inputs-valid-test \
+    	    --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
+          --source-lang eng --target-lang slk \
+          --beam 5   > "$OUTDIR"/test_engslk.log
+
+python fairseq_cli/generate.py data-bin/ted_8_related/ \
+          --task multilingual_translation \
+          --gen-subset test \
+          --path "$OUTDIR"/checkpoint_best.pt \
+          --batch-size 32 \
+          --lenpen 1.0 \
+          --remove-bpe sentencepiece \
+	        --sacrebleu \
+          --encoder-langtok tgt \
+	        --skip-invalid-size-inputs-valid-test \
+    	    --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
+          --source-lang eng --target-lang ces \
+          --beam 5   > "$OUTDIR"/test_engces.log
+
