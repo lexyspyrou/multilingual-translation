b8c8f81062f870568a3041f94d9209f7e9047350
lexyspyrou <alexandra.spyrou@outlook.com>
2022-07-11 16:20:13 +0100

    small change in temperature.sh to run on original size

--- job_scripts/related_ted8_o2m/temperature.sh
+++ job_scripts/related_ted8_o2m/temperature.sh
@@ -15,10 +15,10 @@ echo 'slurm id '$SLURM_JOB_ID >> $MODEL_DIR/train.log
 python train.py data-bin/ted_8_related/ \
 	  --task multilingual_translation \
 	  --arch multilingual_transformer_iwslt_de_en \
-	  --max-epoch 1  \
-    --lang-pairs "eng-aze,eng-tur" \
+	  --max-epoch 40  \
+    --lang-pairs "eng-aze,eng-tur,eng-bel,eng-rus,eng-glg,eng-por,eng-slk,eng-ces" \
     --no-epoch-checkpoints \
-	  --distributed-world-size 4 \
+	  --distributed-world-size 2 \
 	  --encoder-langtok "tgt" \
 	  --share-decoder-input-output-embed --share-decoders --share-encoders \
 	  --dropout 0.3 --attention-dropout 0.3 --relu-dropout 0.3 --weight-decay 0.0 \
@@ -26,7 +26,7 @@ python train.py data-bin/ted_8_related/ \
 	  --optimizer 'adam' --adam-betas '(0.9, 0.98)' --lr-scheduler 'inverse_sqrt' \
 	  --warmup-init-lr 1e-7 --warmup-updates 4000 --lr 2e-4 \
 	  --criterion 'label_smoothed_cross_entropy' --label-smoothing 0.1 \
-	  --max-tokens 500 \
+	  --max-tokens 3072 \
 	  --update-freq 2 \
 	  --seed 2 \
     --max-source-positions 150 --max-target-positions 150 \
@@ -35,9 +35,3 @@ python train.py data-bin/ted_8_related/ \
 	  --log-interval 100 >> $MODEL_DIR/train.log 2>&1 \
     --skip-invalid-size-inputs-valid-test \
     --ddp-backend=no_c10d \
-    --encoder-layers 2 \
-    --decoder-layers 2 \
-    --encoder-embed-dim 32 \
-    --decoder-embed-dim 32 \
-    --encoder-ffn-embed-dim 64 \
-    --decoder-ffn-embed-dim 64
